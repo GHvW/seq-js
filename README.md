ae4603c26fd96d301fd0ddecc58c0c5241a319d0
ghvw <ghvanw283@gmail.com>
2018-09-11 22:52:30 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # seq-js
 Attempt to use generators to create lazy iterators. Similar to the Java 8 Streams API, C# Enumerable, Rust iterator, F# and Kotlin Sequences, etc.
+
+For a fully featured library with similar aims, try out sequency https://github.com/winterbe/streamjs or streamjs https://github.com/winterbe/streamjs
---
+++ iterator.js
@@ -0,0 +1,59 @@
+// Don't use this! extending base types is bad practice!
+// Array.prototype.iter = function() {
+//   return iterator(this);
+// }
+
+function Seq() {};
+
+Seq.of = function(args) {
+  return iterator(args);
+}
+
+function* iterator(iterable) {
+  yield* iterable;
+}
+
+iterator.prototype.map = function(fn) {
+  return mapIter(fn, this);
+}
+
+iterator.prototype.filter = function(predicate) {
+  return filtrator(predicate, this);
+}
+//cant make these lambdas because it doesn't bind this? "this" would be the window?
+//***********Terminal Operations****************************************** */
+iterator.prototype.collect = function() {
+  return [...this];
+}
+
+iterator.prototype.count = function() {
+  let result = 0;
+  while (!this.next().done) {
+    result++
+  }
+  return result
+}
+
+//test
+iterator.prototype.forEach = function(fn) {
+  for (let val of this) {
+    fn(val);
+  }
+}
+
+//*******************Iterators************************* */
+function* mapIter(fn, iterable) {
+  for (let val of iterable) {
+      yield fn(val);
+  }
+}
+mapIter.prototype = Object.create(iterator.prototype);
+
+function* filtrator(predicate, iterable) {
+  for (let val of iterable) {
+      if (predicate(val)) {
+          yield val;
+      }
+  }
+}
+filtrator.prototype = Object.create(iterator.prototype);
